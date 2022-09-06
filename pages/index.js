e55800744126d47f1e7b239c5ebe7752811b995d
Jim Cooper <jim@cooperplanet.com>
2022-09-06 14:31:29 -0600

    Cleanup and remove css module

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,19 @@
-import styles from './Home.module.css';
-
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>
+    <div className="container">
+      <h1 className="title">
         Welcome to the{' '}
-        <span className={styles.playground}>Next.js Playground!</span>
+        <span className="playground">Next.js Playground!</span>
       </h1>
 
-      <p className={styles.description}>
+      <p className="getStarted">
         Get started by editing{' '}
-        <code className={styles.code}>pages/index.js</code>
+        <code className="code">pages/index.js</code>
       </p>
 
-      <p className={styles.description}>
-        <div>This playground is for use with the</div>
-        <div>Pluralsight React with Next.js Playbook Course</div>
+      <p className="description">
+        This playground is for use with the
+        Pluralsight React with Next.js Playbook Course
       </p>
     </div>
   );
